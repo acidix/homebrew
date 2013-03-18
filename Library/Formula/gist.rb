7e85583cb3f515708518f678ee6e949cbb3ff0d3
acidix <thomas.uhde@acidix.net>
2013-03-18 11:35:55 +0100

    Changed gist formula to use the git-master tarball. This is mainly to include a 4-month-old patch that prevents the current gist on homebrew from working.

--- Library/Formula/gist.rb
+++ Library/Formula/gist.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Gist < Formula
   homepage 'https://github.com/defunkt/gist'
-  url 'https://github.com/defunkt/gist/tarball/v3.1.0'
+  url 'https://github.com/defunkt/gist/archive/master.tar.gz'
   sha1 '3271be7d41ac545afd17772652dda7adc7c90d7a'
   head 'https://github.com/defunkt/gist.git'
 
